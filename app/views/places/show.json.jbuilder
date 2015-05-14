728cda7807f59dedf03fbbf45b07fdc5e49ecf36
Peter Armstrong <peter@xap.ie>
2015-05-14 08:34:40 +0100

    places added

---
+++ app/views/places/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @place, :id, :name, :address, :lattitude, :longitude, :description, :picture, :user_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514072746) do
+ActiveRecord::Schema.define(version: 20150514073319) do
+
+  create_table "places", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.decimal  "lattitude"
+    t.decimal  "longitude"
+    t.text     "description"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
